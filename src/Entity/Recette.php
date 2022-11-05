ba1ade355c2f62c1f156db77a7464bb45906322c
Helmi74130 <88458882+Helmi74130@users.noreply.github.com>
2022-11-05 13:21:19 +0100

    make entity

---
+++ src/Entity/Recette.php
@@ -0,0 +1,231 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RecetteRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RecetteRepository::class)]
+class Recette
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?float $preparationTime = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $cuissonTime = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $reposTime = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $etapes = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $note = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $photo = null;
+
+    #[ORM\ManyToMany(targetEntity: ingredient::class, inversedBy: 'recettes')]
+    private Collection $ingredients;
+
+    #[ORM\ManyToMany(targetEntity: regime::class, inversedBy: 'recettes')]
+    private Collection $regimes;
+
+    #[ORM\ManyToMany(targetEntity: allergen::class, inversedBy: 'recettes')]
+    private Collection $allergens;
+
+    public function __construct()
+    {
+        $this->ingredients = new ArrayCollection();
+        $this->regimes = new ArrayCollection();
+        $this->allergens = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPreparationTime(): ?float
+    {
+        return $this->preparationTime;
+    }
+
+    public function setPreparationTime(float $preparationTime): self
+    {
+        $this->preparationTime = $preparationTime;
+
+        return $this;
+    }
+
+    public function getCuissonTime(): ?float
+    {
+        return $this->cuissonTime;
+    }
+
+    public function setCuissonTime(?float $cuissonTime): self
+    {
+        $this->cuissonTime = $cuissonTime;
+
+        return $this;
+    }
+
+    public function getReposTime(): ?float
+    {
+        return $this->reposTime;
+    }
+
+    public function setReposTime(?float $reposTime): self
+    {
+        $this->reposTime = $reposTime;
+
+        return $this;
+    }
+
+    public function getEtapes(): ?string
+    {
+        return $this->etapes;
+    }
+
+    public function setEtapes(string $etapes): self
+    {
+        $this->etapes = $etapes;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getPhoto(): ?string
+    {
+        return $this->photo;
+    }
+
+    public function setPhoto(?string $photo): self
+    {
+        $this->photo = $photo;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ingredient>
+     */
+    public function getIngredients(): Collection
+    {
+        return $this->ingredients;
+    }
+
+    public function addIngredient(ingredient $ingredient): self
+    {
+        if (!$this->ingredients->contains($ingredient)) {
+            $this->ingredients->add($ingredient);
+        }
+
+        return $this;
+    }
+
+    public function removeIngredient(ingredient $ingredient): self
+    {
+        $this->ingredients->removeElement($ingredient);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, regime>
+     */
+    public function getRegimes(): Collection
+    {
+        return $this->regimes;
+    }
+
+    public function addRegime(regime $regime): self
+    {
+        if (!$this->regimes->contains($regime)) {
+            $this->regimes->add($regime);
+        }
+
+        return $this;
+    }
+
+    public function removeRegime(regime $regime): self
+    {
+        $this->regimes->removeElement($regime);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, allergen>
+     */
+    public function getAllergens(): Collection
+    {
+        return $this->allergens;
+    }
+
+    public function addAllergen(allergen $allergen): self
+    {
+        if (!$this->allergens->contains($allergen)) {
+            $this->allergens->add($allergen);
+        }
+
+        return $this;
+    }
+
+    public function removeAllergen(allergen $allergen): self
+    {
+        $this->allergens->removeElement($allergen);
+
+        return $this;
+    }
+}
